0df7cbf49a74b0d7182e21585e7096cecfbf6378
Leonidasacosta <89566738+Leonidasacosta@users.noreply.github.com>
2021-09-02 00:39:01 -0400

    Completed 2.8 Video!

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Leonidas Acosta on 8/29/21.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,13 +14,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        messageLabel.text = ""
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image0")
+        let awesomeMessage = "You Are Amazing!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
+        
+        
+        if messageLabel.text == awesomeMessage {
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        } else if messageLabel.text == greatMessage {
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "image2")
+        } else {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
     }
-    
-}
 
+}
